91ec539b21e28a456f7be787abb8196dda22cf28
Onur Aslan <aslanon.tr@gmail.com>
2019-10-11 14:38:16 +0300

    deploy

---
+++ precache-manifest.13d995d44a915f94a5f82c31b08e49ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/2048/robots.txt"
+  },
+  {
+    "revision": "7a383120dc3251a2096e",
+    "url": "/2048/js/chunk-vendors.a89911a2.js"
+  },
+  {
+    "revision": "c2c1db6a7f5efe5810ea",
+    "url": "/2048/js/app.6af1a24a.js"
+  },
+  {
+    "revision": "4a7ebcbef578ec0af96f53185da444a2",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "c2c1db6a7f5efe5810ea",
+    "url": "/2048/css/app.3d35baa3.css"
+  }
+];
